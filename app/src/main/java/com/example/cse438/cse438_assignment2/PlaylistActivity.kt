485097f9ca730580d299f42ed4fc8bcdb04af6bc
Zedong Xun <zxun@wustl.edu>
2020-02-27 23:22:41 -0600

    hhhhh

--- app/src/main/java/com/example/cse438/cse438_assignment2/PlaylistActivity.kt
+++ app/src/main/java/com/example/cse438/cse438_assignment2/PlaylistActivity.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import com.example.cse438.cse438_assignment2.Fragment.selectionFragment
 import kotlinx.android.synthetic.main.activity_playlist.*
 import kotlinx.android.synthetic.main.create_playlist.*
 import kotlinx.android.synthetic.main.create_playlist.view.*
@@ -16,7 +15,6 @@ import kotlinx.android.synthetic.main.create_playlist.view.*
 class PlaylistActivity : AppCompatActivity() {
     val playlistList: ArrayList<Playlist> = ArrayList()
     val trackInPlaylist: ArrayList<Playlist> = ArrayList()
-    public lateinit var createPlaylistButton: Button
 
 
     private var playlistViewModel: PlaylistViewModel? = null
@@ -32,23 +30,6 @@ class PlaylistActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_playlist)
 
-        //Get the info from the main activity
-        val intent = intent
-        trackTitle = intent!!.getStringExtra("trackTitle")
-        trackArtistName = intent!!.getStringExtra("trackArtistName")
-        trackAlbumTitle = intent!!.getStringExtra("trackAlbumTitle")
-        trackRank = intent!!.getIntExtra("trackRank", 0)
-        trackDuration = intent!!.getIntExtra("trackDuration", 0)
-        trackAlbumCover = intent!!.getStringExtra("trackAlbumCover")
-
-        //set intial fragment
-        val fragment =
-            selectionFragment()
-        var bundle = Bundle()
-        fragment.arguments = bundle
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container, fragment)
-        transaction.commit()
     }
 
     override fun onStart() {
